e630fc7a5b5740d573161a575fcf3c383e25a462
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-02-26 17:30:56 +0530

    Add files via upload

---
+++ google_compute_firewall/outputs.tf
@@ -0,0 +1,6 @@
+output "firewall_name" {
+  value = google_compute_firewall.firewall.name
+}
+output "firewall_id" {
+  value = google_compute_firewall.firewall.id
+}
---
+++ google_compute_firewall/variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+  
+}
+variable "compute_network_name" {
+  
+}
+variable "protocol" {
+# type = list(string)
+}
+variable "ports" {
+    type = list(number)
+}
+variable "source_ranges" {
+  
+}
+variable "target_tags" {
+  
+}
