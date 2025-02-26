96749327b2436e176d8c916847402203a3d536e1
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-02-26 17:32:29 +0530

    Add files via upload

---
+++ google_compute_instance_private/outputs.tf
@@ -0,0 +1,3 @@
+output "compute_instance_id" {
+  value = google_compute_instance.default.id
+}
---
+++ google_compute_instance_private/variables.tf
@@ -0,0 +1,46 @@
+variable "name" {
+  
+}
+variable "machine_type" {
+  
+}
+variable "zone" {
+  
+}
+variable "tags" {
+  
+}
+variable "image" {
+  
+}
+variable "labels" {
+
+}
+variable "metadata" {
+  
+}
+variable "compute_network_name" {
+  
+}
+variable "firewall_name" {
+  
+}
+variable "deletion_protection" {
+  
+}
+variable "size" {
+  
+}
+variable "type" {
+    default = "pd-balanced"
+  
+}
+variable "disk_auto_delete" {
+  
+}
+variable "subnet_name" {
+  
+}
+variable "provisioning_model" {
+  default = "STANDARD"
+}
