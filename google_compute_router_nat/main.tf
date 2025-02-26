272599ca7a5851b9effd81eeb2d0c8e3a3d0b7d6
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-02-26 17:37:19 +0530

    Create main.tf

---
+++ google_compute_router_nat/main.tf
@@ -0,0 +1,18 @@
+resource "google_compute_router_nat" "nat" {
+  name                               = var.name
+  router                             = var.compute_router_name
+  region                             = var.region
+  nat_ip_allocate_option             = var.nat_ip_allocate_option
+  source_subnetwork_ip_ranges_to_nat = var.source_subnetwork_ip_ranges_to_nat
+
+  subnetwork {
+    name                    = var.subnet_id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+  depends_on = [var.compute_router_name]
+}
