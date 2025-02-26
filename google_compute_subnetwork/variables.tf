cf1b4c89339055bf2007e2ec7a5c15924beb1f37
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-02-26 17:38:38 +0530

    Add files via upload

---
+++ google_compute_subnetwork/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {}
+
+variable "region" {}
+
+variable "compute_network_name" {}
+
+variable "name" {}
+
+variable "subnet_cidr_range" {}
+
+variable "private_ip_google_access" {}
+
+variable "compute_network" {
+  description = "Name of the compute network"
+}
