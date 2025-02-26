f97e81973121f881747fce502a56218c8fb8c6f3
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-02-26 17:36:04 +0530

    Create main.tf

---
+++ google_compute_router/main.tf
@@ -0,0 +1,7 @@
+resource "google_compute_router" "router" {
+  project    = var.project_id
+  name       = var.compute_router_name
+  network    = var.compute_network_name
+  region     = var.region
+  depends_on = [var.compute_network]
+}
