06bbaa2b2636cdec6d8b8f9eae762187f5fbc6f9
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-02-26 17:33:48 +0530

    Add files via upload

---
+++ google_compute_instance_public/variables.tf
@@ -0,0 +1,50 @@
+variable "name" {
+  
+}
+variable "machine_type" {
+  
+}
+variable "zone" {
+  
+}
+variable "tags" {
+  
+}
+variable "image" {
+  
+}
+variable "labels" {
+  
+}
+variable "metadata" {
+  
+}
+variable "compute_network_name" {
+  
+}
+variable "firewall_name" {
+  
+}
+variable "deletion_protection" {
+  
+}
+variable "size" {
+  
+}
+variable "type" {
+    default = "pd-balanced"
+  
+}
+variable "disk_auto_delete" {
+  
+}
+variable "subnet_name" {
+  
+}
+variable "provisioning_model" {
+  default = "STANDARD"
+}
+# variable "external_ip" {
+#   description = "Controls if VM gets external IP"
+#   default     = null
+# }
