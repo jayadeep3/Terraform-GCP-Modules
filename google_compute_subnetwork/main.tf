aea7c70371baea0165be940840fc2d9e235ce480
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-02-26 17:38:22 +0530

    Create main.tf

---
+++ google_compute_subnetwork/main.tf
@@ -0,0 +1,9 @@
+resource "google_compute_subnetwork" "subnet" {
+  project                  = var.project_id
+  name                     = var.name
+  ip_cidr_range            = var.subnet_cidr_range
+  region                   = var.region
+  network                  = var.compute_network_name
+  private_ip_google_access = var.private_ip_google_access
+  depends_on               = [var.compute_network]
+}
