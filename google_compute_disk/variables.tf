5bb443cb118598cfbda9dcfe11b11eaf68c51be3
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-02-26 17:29:21 +0530

    Add files via upload

---
+++ google_compute_disk/variables.tf
@@ -0,0 +1,22 @@
+variable "zone" {
+  
+}
+variable "name" {
+  
+}
+variable "type" {
+  
+}
+variable "image" {
+  
+}
+variable "labels" {
+  
+}
+#variable "physical_block_size_bytes" {
+  
+#}
+variable "size" {
+  
+}
+
