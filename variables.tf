e735433012eb6ace01c945a4409684adaef451a5
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-19 14:18:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,73 +1,181 @@
-variable "project_id" { description = "GCP project ID" }
-variable "region"     { description = "GCP region" }
-variable "zone"       { description = "GCP zone" }
+# Global/GCP project settings
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+variable "region" {
+  description = "Region for resources"
+  type        = string
+}
 
-# Network module
-variable "network_name" {}
-variable "auto_create_subnetworks" {}
-variable "routing_mode" {}
+# Network
+variable "network" {
+  description = "VPC network name"
+  type        = string
+}
+variable "subnetwork" {
+  description = "Subnetwork name"
+  type        = string
+}
+variable "auto_create_subnetworks" {
+  description = "Auto create subnetworks"
+  type        = bool
+}
+variable "routing_mode" {
+  description = "Routing mode"
+  type        = string
+}
 
-# Subnetwork module
-variable "subnetwork_name" {}
-variable "subnet_cidr_range" {}
-variable "private_ip_google_access" {}
+# Subnetwork
+variable "subnet_cidr_range" {
+  description = "CIDR range for subnet"
+  type        = string
+}
+variable "private_ip_google_access" {
+  description = "Enable private Google access"
+  type        = bool
+}
 
-# Firewall module
-variable "firewall_name" {}
-variable "firewall_protocol" {}
-variable "firewall_ports" { type = list(number) }
-variable "firewall_source_ranges" {}
-variable "firewall_target_tags" {}
+# Compute Network/Firewall
+variable "compute_network_name" {
+  description = "Compute network name"
+  type        = string
+}
+variable "protocol" {
+  description = "Firewall protocol"
+  type        = string
+}
+variable "ports" {
+  description = "List of allowed ports"
+  type        = list(string)
+}
+variable "source_ranges" {
+  description = "Source ranges for firewall"
+  type        = list(string)
+}
+variable "target_tags" {
+  description = "Target tags for firewall"
+  type        = list(string)
+}
 
-# Router module
-variable "router_name" {}
+# Compute disk
+variable "name" {
+  description = "Name (for disk, instances, etc.)"
+  type        = string
+}
+variable "type" {
+  description = "Disk type"
+  type        = string
+}
+variable "zone" {
+  description = "Zone for resources"
+  type        = string
+}
+variable "image" {
+  description = "Image for disk or instance"
+  type        = string
+}
+variable "size" {
+  description = "Size of the disk"
+  type        = number
+}
+variable "labels" {
+  description = "Labels for resources"
+  type        = map(string)
+}
 
-# Router NAT module
-variable "nat_name" {}
-variable "nat_ip_allocate_option" {}
-variable "source_subnetwork_ip_ranges_to_nat" {}
-variable "nat_subnet_id" {}
+# Compute Instance (private/public)
+variable "machine_type" {
+  description = "Machine type"
+  type        = string
+}
+variable "deletion_protection" {
+  description = "Enable deletion protection"
+  type        = bool
+}
+variable "tags" {
+  description = "Tags for instance"
+  type        = list(string)
+}
+variable "disk_auto_delete" {
+  description = "Auto-delete boot disk"
+  type        = bool
+}
+variable "provisioning_model" {
+  description = "Provisioning model"
+  type        = string
+}
+variable "metadata" {
+  description = "Metadata for instance"
+  type        = map(string)
+}
+variable "firewall_name" {
+  description = "Firewall rule name"
+  type        = string
+}
+variable "service_account_email" {
+  description = "Service account email"
+  type        = string
+}
+variable "subnet_name" {
+  description = "Subnet name"
+  type        = string
+}
 
-# Disk module
-variable "disk_name" {}
-variable "disk_type" {}
-variable "disk_image" {}
-variable "disk_labels" {}
-variable "disk_size" {}
+# Attached Disk
+variable "disk_id" {
+  description = "Disk ID to attach"
+  type        = string
+}
+variable "compute_instance_id" {
+  description = "Compute instance ID for disk attachment"
+  type        = string
+}
+variable "instance_zone" {
+  description = "Zone of the instance for disk attachment"
+  type        = string
+}
 
-# Attached Disk module
-variable "disk_id" {}
-variable "compute_instance_id" {}
+# Router
+variable "compute_router_name" {
+  description = "Router name"
+  type        = string
+}
 
-# Instance Private module
-variable "instance_private_name" {}
-variable "instance_private_machine_type" {}
-variable "instance_private_tags" {}
-variable "instance_private_image" {}
-variable "instance_private_labels" {}
-variable "instance_private_metadata" {}
-variable "instance_private_deletion_protection" {}
-variable "instance_private_disk_size" {}
-variable "instance_private_disk_type" {}
-variable "instance_private_disk_auto_delete" {}
-variable "instance_private_provisioning_model" {}
+# Router NAT
+variable "nat_ip_allocate_option" {
+  description = "NAT IP allocation option"
+  type        = string
+}
+variable "source_subnetwork_ip_ranges_to_nat" {
+  description = "Source subnetwork IP ranges to NAT"
+  type        = string
+}
+variable "subnet_id" {
+  description = "Subnet ID for NAT"
+  type        = string
+}
 
-# Instance Public module
-variable "instance_public_name" {}
-variable "instance_public_machine_type" {}
-variable "instance_public_tags" {}
-variable "instance_public_image" {}
-variable "instance_public_labels" {}
-variable "instance_public_metadata" {}
-variable "instance_public_deletion_protection" {}
-variable "instance_public_disk_size" {}
-variable "instance_public_disk_type" {}
-variable "instance_public_disk_auto_delete" {}
-variable "instance_public_provisioning_model" {}
+# GKE Cluster
+variable "cluster_name" {
+  description = "GKE cluster name"
+  type        = string
+}
 
-variable "cluster_name" { default = "my-gke-cluster" }
-variable "nodepool_name" { default = "primary-node-pool" }
-variable "node_count" { default = 1 }
-variable "machine_type" { default = "e2-medium" }
-variable "node_labels" { default = {} }
-variable "preemptible" { default = false }
+# GKE Nodepool
+variable "nodepool_name" {
+  description = "Node pool name"
+  type        = string
+}
+variable "node_count" {
+  description = "Number of nodes in node pool"
+  type        = number
+}
+variable "node_labels" {
+  description = "Labels for GKE node pool"
+  type        = map(string)
+}
+variable "preemptible" {
+  description = "Use preemptible VMs"
+  type        = bool
+}
