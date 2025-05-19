0e3fad5c77d32ed291a038ff0681e1a9bb422ad8
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-19 12:10:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,27 @@
-# Output example - adjust as needed for your modules
 output "network_name" {
   value = module.network.name
 }
-# Add more outputs as needed
+output "subnetwork_name" {
+  value = module.subnetwork.name
+}
+output "firewall_name" {
+  value = module.firewall.name
+}
+output "router_name" {
+  value = module.router.name
+}
+output "nat_name" {
+  value = module.router_nat.name
+}
+output "disk_name" {
+  value = module.disk.name
+}
+output "attached_disk_id" {
+  value = module.attached_disk.disk_id
+}
+output "instance_private_name" {
+  value = module.instance_private.name
+}
+output "instance_public_name" {
+  value = module.instance_public.name
+}
