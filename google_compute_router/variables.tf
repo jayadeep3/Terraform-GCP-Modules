fe3f6ce05454c82bec633c38e02ad28a003d8d7b
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-22 10:44:12 +0530

    Update variables.tf

--- google_compute_router/variables.tf
+++ google_compute_router/variables.tf
@@ -1,8 +1,8 @@
 variable "project_id" {}
 variable "compute_network_name" {}
-variable "compute_network" {
-  description = "Name of the compute network"
-}
+#variable "compute_network" {
+#  description = "Name of the compute network"
+#}
 variable "compute_router_name" {
 
 }
