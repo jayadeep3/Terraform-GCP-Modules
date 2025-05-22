531c2a7d529accc08ebc6a2bc4e83ecb1a828399
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-22 10:55:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,32 @@
 output "network_name" {
-  value = module.network.name
+  value = module.network.compute_network_name
 }
-output "subnetwork_name" {
-  value = module.subnetwork.name
+output "public_subnetwork_name" {
+  value = module.public_subnet.subnets_name
+}
+output "private_subnetwork_name" {
+  value = module.private_subnet.subnets_name
 }
 output "firewall_name" {
-  value = module.firewall.name
+  value = module.firewall.firewall_name
 }
 output "router_name" {
-  value = module.router.name
+  value = module.router.cloud_router_name
 }
 output "nat_name" {
-  value = module.router_nat.name
+  value = module.router_nat.router_nat_name
 }
 output "disk_name" {
-  value = module.disk.name
+  value = module.disk.disk_id
 }
 output "attached_disk_id" {
-  value = module.attached_disk.disk_id
+  value = module.attached_disk.attached_disk_name
 }
 output "instance_private_name" {
-  value = module.instance_private.name
+  value = module.instance_private.compute_instance_id
 }
 output "instance_public_name" {
-  value = module.instance_public.name
+  value = module.instance_public.compute_instance_id
 }
 output "gke_cluster_id" {
   value = module.gke_cluster.cluster_id
