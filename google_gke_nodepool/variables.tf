faca646841be50ed2b15fb10260a1047ec789c76
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-22 11:35:59 +0530

    Update variables.tf

--- google_gke_nodepool/variables.tf
+++ google_gke_nodepool/variables.tf
@@ -1,10 +1,10 @@
-variable "name" {}
-variable "location" {}
-variable "project_id" {}
-variable "cluster" {}
-variable "node_count" { default = 1 }
-variable "machine_type" { default = "e2-medium" }
-variable "labels" { default = {} }
-variable "preemptible" { default = false }
-variable "disk_type" {}
-variable "disk_size_gb" {}
+#variable "name" {}
+#variable "location" {}
+#variable "project_id" {}
+#variable "cluster" {}
+#variable "node_count" { default = 1 }
+#variable "machine_type" { default = "e2-medium" }
+#variable "labels" { default = {} }
+#variable "preemptible" { default = false }
+#variable "disk_type" {}
+#variable "disk_size_gb" {}
