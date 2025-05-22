04ddb22c5661921adbbf3a45a22c5d24612d6a01
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-22 11:35:08 +0530

    Update main.tf

--- google_gke_nodepool/main.tf
+++ google_gke_nodepool/main.tf
@@ -1,19 +1,19 @@
-resource "google_container_node_pool" "default" {
-  name       = var.name
-  location   = var.location
-  cluster    = var.cluster
-  project    = var.project_id
+#resource "google_container_node_pool" "default" {
+#  name       = var.name
+#  location   = var.location
+#  cluster    = var.cluster
+#  project    = var.project_id
 
-  node_count = var.node_count
+#  node_count = var.node_count
 
-  node_config {
-    machine_type = var.machine_type
-    labels       = var.labels
-    preemptible  = var.preemptible
-    disk_type    = var.disk_type        # Add this line
-    disk_size_gb = var.disk_size_gb                   # Or set lower size like 50 GB
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
-}
+#  node_config {
+#    machine_type = var.machine_type
+#    labels       = var.labels
+#    preemptible  = var.preemptible
+#    disk_type    = var.disk_type        # Add this line
+#    disk_size_gb = var.disk_size_gb                   # Or set lower size like 50 GB
+#    oauth_scopes = [
+#      "https://www.googleapis.com/auth/cloud-platform",
+#    ]
+#  }
+#}
