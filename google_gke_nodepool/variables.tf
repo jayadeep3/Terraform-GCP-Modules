85b25b2af8e839e5c3d70f575c8cb82f9663aee4
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-22 10:52:11 +0530

    Update variables.tf

--- google_gke_nodepool/variables.tf
+++ google_gke_nodepool/variables.tf
@@ -6,3 +6,5 @@ variable "node_count" { default = 1 }
 variable "machine_type" { default = "e2-medium" }
 variable "labels" { default = {} }
 variable "preemptible" { default = false }
+variable "disk_type" {}
+variable "disk_size_gb" {}
