576a458589e6cbad6ee3ff6b018d75e001162f38
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-22 10:49:52 +0530

    Create variables.tf

---
+++ google_compute_public_subnetwork/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {}
+
+variable "region" {}
+
+variable "compute_network_name" {}
+
+variable "name" {}
+
+variable "public_subnet_cidr_range" {}
+variable "private_ip_google_access" {}
+
+# variable "compute_network" {
+#   description = "Name of the compute network"
+# }
