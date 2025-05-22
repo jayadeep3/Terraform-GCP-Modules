4bbd6277d7a0d566282884225c0c843e1cc65224
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-22 11:34:31 +0530

    Update variables.tf

--- google_gke_cluster/variables.tf
+++ google_gke_cluster/variables.tf
@@ -1,5 +1,5 @@
-variable "name" {}
-variable "location" {}
-variable "project_id" {}
-variable "network" {}
-variable "subnetwork" {}
+#variable "name" {}
+#variable "location" {}
+#variable "project_id" {}
+#variable "network" {}
+#variable "subnetwork" {}
