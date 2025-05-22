f6d06a88ed89487a6b51210407493d945b735b8b
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-22 11:34:13 +0530

    Update outputs.tf

--- google_gke_cluster/outputs.tf
+++ google_gke_cluster/outputs.tf
@@ -1,9 +1,9 @@
-output "cluster_id" {
-  value = google_container_cluster.primary.id
-}
-output "endpoint" {
-  value = google_container_cluster.primary.endpoint
-}
-output "master_version" {
-  value = google_container_cluster.primary.master_version
-}
+#output "cluster_id" {
+#  value = google_container_cluster.primary.id
+#}
+#output "endpoint" {
+#  value = google_container_cluster.primary.endpoint
+#}
+#output "master_version" {
+#  value = google_container_cluster.primary.master_version
+#}
