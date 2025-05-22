1f6605c8df4fa8aa0adc782fd1da61d9fcb4bf31
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-05-22 11:33:19 +0530

    Update main.tf

--- google_gke_cluster/main.tf
+++ google_gke_cluster/main.tf
@@ -1,13 +1,13 @@
-resource "google_container_cluster" "primary" {
-  name     = var.name
-  location = var.location
-  project  = var.project_id
+#resource "google_container_cluster" "primary" {
+#  name     = var.name
+#  location = var.location
+#  project  = var.project_id
 
-  network    = var.network
-  subnetwork = var.subnetwork
+#  network    = var.network
+#  subnetwork = var.subnetwork
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
+#  remove_default_node_pool = true
+#  initial_node_count       = 1
 
-  ip_allocation_policy {}
-}
+#  ip_allocation_policy {}
+#}
